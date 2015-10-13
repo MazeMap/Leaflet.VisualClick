b13066f2e76697003f9f1211f7597055f7d4b610
Dag Jomar Mersland <dagjomar@gmail.com>
2015-10-13 16:39:40 +0200

    rewriting to a leaflet handler type plugin

--- L.VisualClick.js
+++ L.VisualClick.js
@@ -16,89 +16,55 @@ var iconPulsing = L.divIcon({
     html: '<i class="'+ (L.Browser.touch ? 'touchpulse' : 'pulse') +'"></i>'
 });
 
+L.visualClick = function (options) {
+    return new L.VisualClick(options);
+};
 
-L.VisualClick = L.Class.extend({
-    options: {
-        events: 'click contextmenu', //Using ordinary Leaflet syntax for space-seperated event names
-        map: null,
-        disabled: false, //This flag can be set to disable any VisualClicks indefinately until this flag is set to true
-        supportsTransitions: null, //Will be checked upon initialize unless explicitly set
-        removeTimeout: 690 //Default, but can be changed if the icon pulse animation timing is changed
-    },
 
-    /* http://stackoverflow.com/questions/7264899/detect-css-transitions-using-javascript-and-without-modernizr */
-    _supportsTransitions: function(){
-        var b = document.body || document.documentElement,
-            s = b.style,
-            p = 'transition';
+L.Map.VisualClick = L.Handler.extend({
 
-        if (typeof s[p] === 'string') { return true; }
+    _pulse: iconPulsing,
 
-        // Tests for vendor specific prop
-        var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
-        p = p.charAt(0).toUpperCase() + p.substr(1);
+    _eventName: 'click',
 
-        for (var i=0; i<v.length; i++) {
-            if (typeof s[v[i] + p] === 'string') { return true; }
-        }
+    _onClick: function(e) {
 
-        return false;
-    },
-
-    initialize: function (options) {
-        this.options = L.setOptions(this, options);
-
-        if(this.options.supportsTransitions === null){
-            this.options.supportsTransitions = this._supportsTransitions();
-        }
-
-        if(this.options.map){
-            this.options.map.visualClick = this; //Store a reference to the visualClick object in the given map
-            this._setUpHandlers(this.options.map);
-        }
-    },
-
-    _setUpHandlers : function(map){
-        if(this.options.events.length > 0){
-            map.on(this.options.events, this._handleClick, this);
-        }
-    },
-
-    _removeHandlers : function(map){
-        map.off('click', this._handleClick);
-        map.off('contextmenu', this._handleClick);
-    },
-
-    _handleClick : function(e){
-
-        if(this.options.disabled){
-            return;
-        }else if(this.options.ignoreNextClick){
-            this.options.ignoreNextClick = false; //reset
-            return;
-        }else if(e.noVisualClick){ //Can also check to see if the click event itself has a flag to disable visualClicks
-            return;
-        }
+        var map = this._map;
 
         var latlng = e.latlng;
-        var marker = L.marker(latlng, {pane: 'shadowPane', icon: iconPulsing, interactive: false}).addTo(this.options.map);
+        var marker = L.marker(latlng, {pane: 'shadowPane', icon: iconPulsing, interactive: false}).addTo(map);
 
         //Check for need transition support
-        if(this.options.supportsTransitions === false){
+        if(map.options.visualClick.supportsTransitions === false){
             $(marker._icon).addClass('ielt9').find('i.pulse').css({'width': 30, 'height': 30, 'margin' : '0px 0 0 0px', 'border': '1px solid #62c6e7', 'opacity': 1})
             .animate({width: 60, height: 60, 'margin': '-12px 0 0 -12px', opacity: 0}, 450, 'linear');
         }
         window.setTimeout(function(){
-            if(this.options.map){
-                marker.removeFrom(this.options.map);
+            if(map){
+                marker.removeFrom(map);
             }
-        }.bind(this), this.options.removeTimeout);    // CSS animation is 450 msec long
+        }.bind(this), map.options.visualClick.removeTimeout);    // CSS animation is 450 msec long
 
-    }
+        return true;
+    },
+
+    addHooks: function () {
+        this._map.on('click contextmenu', this._onClick, this);
+    },
+
+    removeHooks: function () {
+        this._map.off('click contextmenu', this._onClick, this);
+    },
 
 });
 
-L.visualClick = function (options) {
-    return new L.VisualClick(options);
-};
+
+L.Map.mergeOptions({
+    visualClick: {
+        supportsTransitions: true,
+        removeTimeout: 690
+    }
+});
+
+L.Map.addInitHook('addHandler', 'visualClick', L.Map.VisualClick);
 
