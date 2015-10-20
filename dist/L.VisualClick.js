1af5e6e6b3888bdf24cae24be1e9c4c0f2385ebe
Dag Jomar Mersland <dagjomar@gmail.com>
2015-10-20 13:15:31 +0200

    Built dist files

--- dist/L.VisualClick.js
+++ dist/L.VisualClick.js
@@ -1,9 +1,9 @@
 /*
- * L.VisualClick
- * Description: A plugin that adds visual feedback when user clicks/taps the map. Useful for when you have a delay on the clickEvents for async fetching of data, or implmentation of Leaflet.singleclick
- * Example: L.visualClick({map: leafletMap}); //Just works
- * Author: Dag Jomar Mersland (twitter: @dagjomar)
- */
+* L.VisualClick
+* Description: A plugin that adds visual feedback when user clicks/taps the map. Useful for when you have a delay on the clickEvents for async fetching of data, or implmentation of Leaflet.singleclick
+* Example: L.visualClick({map: leafletMap}); //Just works
+* Author: Dag Jomar Mersland (twitter: @dagjomar)
+*/
 
 
 L.Map.VisualClick = L.Handler.extend({
@@ -17,7 +17,6 @@ L.Map.VisualClick = L.Handler.extend({
             iconSize: [0, 0],
             clickable: false
         });
-
     },
 
     _visualIcon: null,
@@ -27,7 +26,11 @@ L.Map.VisualClick = L.Handler.extend({
         var map = this._map;
 
         var latlng = e.latlng;
-        var marker = L.marker(latlng, {pane: 'shadowPane', icon: this._visualIcon, interactive: false}).addTo(map);
+        var marker = L.marker(latlng, {
+            pane: this._map.options.visualClickPane,
+            icon: this._visualIcon,
+            interactive: false
+        }).addTo(map);
 
         window.setTimeout(function(){
             if(map){
@@ -42,6 +45,11 @@ L.Map.VisualClick = L.Handler.extend({
         if(this._visualIcon === null){
             this._visualIcon = this._makeVisualIcon();
         }
+
+        if (this._map.options.visualClickPane === 'ie10-visual-click-pane') {
+            this._map.createPane('ie10-visual-click-pane');
+        }
+
         this._map.on(this._map.options.visualClickEvents, this._onClick, this);
     },
 
@@ -56,6 +64,9 @@ L.Map.mergeOptions({
     visualClick: L.Browser.any3d ? true : false, //Can be true, desktop, touch, false. Not straight forward to use L.Browser.touch flag because true on IE10
     visualClickMode: L.Browser.touch && L.Browser.mobile ? 'touch' : 'desktop', //Not straight forward to use only L.Browser.touch flag because true on IE10 - so this is slightly better
     visualClickEvents: 'click contextmenu', //Standard leaflety way of defining which events to hook on to
+    visualClickPane: (L.Browser.ie && document.documentMode === 10) ?
+        'ie10-visual-click-pane' :
+        'shadowPane'	// Map pane where the pulse markers will be showh
 });
 
 L.Map.addInitHook('addHandler', 'visualClick', L.Map.VisualClick);
